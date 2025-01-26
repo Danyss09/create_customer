66a9182aa8db9852df2043e128486101adc64678
Danyss09 <112229585+Danyss09@users.noreply.github.com>
2025-01-25 19:24:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from controllers.customer_controller import customer_bp
+from dotenv import load_dotenv
+import os
+
+# Cargar variables desde .env
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -7,4 +12,8 @@ app = Flask(__name__)
 app.register_blueprint(customer_bp)
 
 if __name__ == '__main__':
+    # Opcional: Verificar que las variables de entorno están cargadas
+    print("DB_HOST:", os.getenv("DB_HOST"))
+    print("DB_PORT:", os.getenv("DB_PORT"))
+    
     app.run(host='0.0.0.0', port=5000)
